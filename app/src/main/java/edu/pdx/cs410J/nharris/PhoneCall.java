5d8fc535174cd7e09bacb6f504ab6f48b5318697
Alex Harris <nharris356087@gmail.com>
2022-08-01 14:38:31 -0700

    caught up in last weeks class with changes/additions

---
+++ app/src/main/java/edu/pdx/cs410J/nharris/PhoneCall.java
@@ -0,0 +1,25 @@
+package edu.pdx.cs410J.nharris;
+
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+public class PhoneCall extends AbstractPhoneCall {
+    @Override
+    public String getCaller() {
+        return null;
+    }
+
+    @Override
+    public String getCallee() {
+        return null;
+    }
+
+    @Override
+    public String getBeginTimeString() {
+        return null;
+    }
+
+    @Override
+    public String getEndTimeString() {
+        return null;
+    }
+}

96a639ef2d1a9b5605a85c329bb4964db9fd9652
Alex Harris <nharris356087@gmail.com>
2022-08-02 20:43:49 -0700

    Updated various things. See diff. Small changes.

--- app/src/main/java/edu/pdx/cs410J/nharris/MainActivity.java
+++ app/src/main/java/edu/pdx/cs410J/nharris/MainActivity.java
@@ -7,26 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.main_activity_ab_title);
+  }
 
-    public void sayHello(View view) {
-        PhoneCall call = new PhoneCall();
-        Toast.makeText(this, "This call is: " + call, Toast.LENGTH_SHORT).show();
-    }
+  public void sayHello(View view) {
+    PhoneCall call = new PhoneCall();
+    Toast.makeText(this, "This call is: " + call, Toast.LENGTH_SHORT).show();
+  }
 
-    public void readmeViewer(View view) {
-        Intent intent = new Intent(this, ReadmeActivity.class);
-        startActivity(intent);
-    }
+  public void readmeViewer(View view) {
+    Intent intent = new Intent(this, ReadmeActivity.class);
+    startActivity(intent);
+  }
 
-    public void customerViewer(View view) {
-        Intent intent = new Intent(this, CustomersActivity.class);
-        startActivity(intent);
-    }
+  public void customerViewer(View view) {
+    Intent intent = new Intent(this, CustomersActivity.class);
+    startActivity(intent);
+  }
 }
--- app/src/main/java/edu/pdx/cs410J/nharris/PhoneCall.java
+++ app/src/main/java/edu/pdx/cs410J/nharris/PhoneCall.java
@@ -3,23 +3,23 @@ package edu.pdx.cs410J.nharris;
 import edu.pdx.cs410J.AbstractPhoneCall;
 
 public class PhoneCall extends AbstractPhoneCall {
-    @Override
-    public String getCaller() {
-        return "123-456-9845";
-    }
+  @Override
+  public String getCaller() {
+    return "123-456-9845";
+  }
 
-    @Override
-    public String getCallee() {
-        return "142-512-3123";
-    }
+  @Override
+  public String getCallee() {
+    return "142-512-3123";
+  }
 
-    @Override
-    public String getBeginTimeString() {
-        return "1/2/2022 1:00 PM";
-    }
+  @Override
+  public String getBeginTimeString() {
+    return "1/2/2022 1:00 PM";
+  }
 
-    @Override
-    public String getEndTimeString() {
-        return "1/2/2022 2:00 PM";
-    }
+  @Override
+  public String getEndTimeString() {
+    return "1/2/2022 2:00 PM";
+  }
 }

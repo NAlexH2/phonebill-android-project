cc2b81f7e0759d772fcf1bf0f2b63d66082b3e86
Alex Harris <nharris356087@gmail.com>
2022-08-02 20:39:37 -0700

    Modified 'logo'. Also Readme to better suit current design.

--- app/src/main/java/edu/pdx/cs410J/nharris/ReadmeActivity.java
+++ app/src/main/java/edu/pdx/cs410J/nharris/ReadmeActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class ReadmeActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_readme);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_readme);
+    Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.readme_activity_ab_title);
+  }
 }

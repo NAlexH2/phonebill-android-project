21e5a85ea14eacee3ca072c1820a6690758fcf24
Alex Harris <nharris356087@gmail.com>
2022-11-02 10:43:51 -0700

    Added comments.

--- app/src/main/java/edu/pdx/cs410J/nharris/ReadmeActivity.java
+++ app/src/main/java/edu/pdx/cs410J/nharris/ReadmeActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 
 import java.util.Objects;
 
+/**
+ * Class containing the necessary contents to display the readme to the user. Required as
+ * part of the assignment.
+ */
 public class ReadmeActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Jump straight into viewing the readme
     setContentView(R.layout.activity_readme);
     Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.readme_activity_ab_title);
   }
